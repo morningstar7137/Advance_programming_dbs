5a38f932b1fc2bce62df5b7c116494ebec4bc258
morningstar7137 <136721285+morningstar7137@users.noreply.github.com>
2026-04-11 02:36:49 +0100

    Update OrderTests.cs

--- Task 1/Churros Order System/Churros Order System/OrderTests.cs
+++ Task 1/Churros Order System/Churros Order System/OrderTests.cs
@@ -1,21 +1,21 @@
-using Microsoft.VisualStudio.TestTools.UnitTesting; // Imports the Unit Testing framework
+using Microsoft.VisualStudio.TestTools.UnitTesting; 
 
-namespace ChurrosApp.Tests                          // Logical namespace for tests
+namespace ChurrosApp.Tests                          
 {
-    [TestClass]                                     // Defines this as a test class
+    [TestClass]                                     
     public class OrderTests                         // Name of the test class
     {
-        [TestMethod]                                // Defines this as a test case
-        public void PayBillTest()                   // Name of the specific test
+        [TestMethod]                                
+        public void PayBillTest()                  
         {
             Churros testItem = new Churros("Test", 5.00); // Sets up test data
             Order testOrder = new Order(99);        // Creates a test order
             testOrder.PlaceOrder(testItem, 3);      // Executes the method to test
             
-            double expected = 15.00;                // Defines expected result (5 * 3)
+            double expected = 15.00;                
             double actual = testOrder.PayBill();    // Gets actual result
 
-            Assert.AreEqual(expected, actual);      // Checks if actual matches expected
+            Assert.AreEqual(expected, actual);      
         }
     }
 }
