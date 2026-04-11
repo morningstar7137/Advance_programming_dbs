8e8d0e984d41ee34bec47a6311fdcd66f4917340
morningstar7137 <136721285+morningstar7137@users.noreply.github.com>
2026-04-11 02:48:36 +0100

    Final Update Que3_client.py

--- Task 3/Que3_client.py
+++ Task 3/Que3_client.py
@@ -1,39 +1,39 @@
-import socket                                     # Imports the socket module for network connections
-import json                                       # Imports the json module to format data
+import socket                                     
+import json                                      
 
-def run_client():                                 # Defines the main client function
+def run_client():                                 
     host = '127.0.0.1'                            # Sets the server address to connect to
     port = 5001                                   # Sets the port number to connect to
     
     client_socket = socket.socket()               # Creates a new TCP socket object
     try:                                          # Starts a try block for exception handling
-        client_socket.connect((host, port))       # Connects to the server
-        print("Successfully connected to the EasyDrive server!\n") # Prints success message
+        client_socket.connect((host, port))       
+        print("Successfully connected to the EasyDrive server!\n") 
         
-        print("--- EasyDrive Registration ---")   # Prints header
-        name = input("Enter Name: ")              # Takes name input from user
-        address = input("Enter Address: ")        # Takes address input from user
-        pps = input("Enter PPS Number: ")         # Takes PPS input from user
-        licence = input("Enter Driving License: ")# Takes license input from user
+        print("--- EasyDrive Registration ---")   
+        name = input("Enter Name: ")              
+        address = input("Enter Address: ")        
+        pps = input("Enter PPS Number: ")        
+        licence = input("Enter Driving License: ")
         
         customer = {                              # Creates a dictionary with customer data
-            "Name": name,                         # Adds name to the dictionary
-            "Address": address,                   # Adds address to the dictionary
-            "PPS": pps,                           # Adds PPS to the dictionary
-            "License": licence                    # Adds license to the dictionary
+            "Name": name,                         
+            "Address": address,                   
+            "PPS": pps,                           
+            "License": licence                    
         }
         
         data = json.dumps(customer)               # Converts the dictionary to a JSON string
-        client_socket.send(data.encode())         # Encodes and sends the JSON data to the server
+        client_socket.send(data.encode())         
         
         response = client_socket.recv(1024).decode() # Receives and decodes the response from server
-        print("\nRegistration Successful!")       # Prints success message
-        print("Your Registration Number: " + response) # Prints the received registration number
+        print("\nRegistration Successful!")       
+        print("Your Registration Number: " + response) 
         
     except Exception as e:                        # Catches any exceptions during connection
-        print("Error connecting to server: " + str(e)) # Prints the error message to the terminal
+        print("Error connecting to server: " + str(e)) 
         
-    client_socket.close()                         # Closes the socket connection
+    client_socket.close()                         
 
-if __name__ == '__main__':                        # Checks if the script is run directly
+if __name__ == '__main__':                        
     run_client()                                  # Calls the run_client function to start
