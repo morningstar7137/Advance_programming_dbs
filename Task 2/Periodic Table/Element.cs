ffce597d155d29655c787904c69b692c8c19d4f2
morningstar7137 <136721285+morningstar7137@users.noreply.github.com>
2026-04-11 02:41:10 +0100

    Update Element.cs

--- Task 2/Periodic Table/Element.cs
+++ Task 2/Periodic Table/Element.cs
@@ -1,12 +1,12 @@
-namespace PeriodicTableApp                                  // Uses the same group name to link files
+namespace PeriodicTableApp                                  
 {
-    public class Element : Matter                           // The ':' makes Element inherit from Matter
+    public class Element : Matter                          
     {
         public string ElementClass { get; private set; }    // Property specific only to the Element class
 
         public Element(int num, string name, string eClass) : base(num, name) // 'base' sends data to the Matter constructor
         {
-            ElementClass = eClass;                          // Assigns the element classification
+            ElementClass = eClass;                          
         }
     }
 }
